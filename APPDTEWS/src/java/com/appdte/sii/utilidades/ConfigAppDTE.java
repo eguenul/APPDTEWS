2773ad75865ef66b9b1bd4e5a3362effef4ef85f
eguenul <eguenul@yahoo.com>
2022-07-20 01:01:28 -0400

    20-07-2022

--- APPDTEWS/src/java/com/appdte/sii/utilidades/ConfigAppDTE.java
+++ APPDTEWS/src/java/com/appdte/sii/utilidades/ConfigAppDTE.java
@@ -6,13 +6,9 @@
 package com.appdte.sii.utilidades;
 
 import java.io.IOException;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
+import java.util.Properties;
 import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -28,7 +24,7 @@ public class ConfigAppDTE {
     private final String pathimg;
     private final String pathenvironment;
     private final String pathtemplate;
-    private final String filepath;
+    
     private final String pathdownload;
     private final String serverauth;
     private final String serveracceptdte;
@@ -37,73 +33,26 @@ public class ConfigAppDTE {
     public ConfigAppDTE() throws ParserConfigurationException, SAXException, IOException{
        
         
-       
-       filepath = "/home/appdte/appdteconf.xml";
-	
-         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-	 DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-	 Document doc = docBuilder.parse(filepath);
-         
-        NodeList nlpathcert = doc.getElementsByTagName("path-certificate");
-        Element elpathcert = (Element) nlpathcert.item(0);
-        this.pathcert = elpathcert.getFirstChild().getNodeValue();
-         
-         
-         
-         
-         
-         NodeList nodepathpdf = doc.getElementsByTagName("path-pdf");
-         Element elpathpdf = (Element) nodepathpdf.item(0);
-         this.pathpdf = elpathpdf.getFirstChild().getNodeValue();
         
-         
-         NodeList nodepathcaf = doc.getElementsByTagName("path-caf");
-         Element elpathcaf = (Element) nodepathcaf.item(0);
-         this.pathcaf = elpathcaf.getFirstChild().getNodeValue();
-         
-         NodeList nodepathdata =  doc.getElementsByTagName("path-data");
-         Element elpathdata = (Element) nodepathdata.item(0);
-         this.pathdata = elpathdata.getFirstChild().getNodeValue(); 
-         
-          NodeList nodepathdte =  doc.getElementsByTagName("path-DTE");
-          Element elpathdte = (Element) nodepathdte.item(0);
-          this.pathdte = elpathdte.getFirstChild().getNodeValue();
-         
-          NodeList nodepathenvironment = doc.getElementsByTagName("environment-url");
-          Element elpathenvironment =  (Element) nodepathenvironment.item(0);
-          this.pathenvironment = elpathenvironment.getFirstChild().getNodeValue();
-          
-          
-          NodeList nodepathimg =  doc.getElementsByTagName("path-img");
-          Element elpathimg =  (Element) nodepathimg.item(0);  
-          this.pathimg = elpathimg.getFirstChild().getNodeValue();
-          
-
-          NodeList nodepathtemplate =  doc.getElementsByTagName("path-template");
-          Element elpathtemplate =  (Element) nodepathtemplate.item(0);  
-          this.pathtemplate = elpathtemplate.getFirstChild().getNodeValue();  
-
-
-          
-          NodeList nodepathdownload =  doc.getElementsByTagName("path-download");
-          Element elpathdownload =  (Element) nodepathdownload.item(0);  
-          this.pathdownload = elpathdownload.getFirstChild().getNodeValue();  
-
-          
-          
-          
-          
-         
-         NodeList nodeserverauth =  doc.getElementsByTagName("server-auth");
-         Element elserverauth =  (Element) nodeserverauth.item(0);  
-         this.serverauth = elserverauth.getFirstChild().getNodeValue();
-         
-         
-         NodeList nodeserveracceptdte =  doc.getElementsByTagName("server-acceptdte");
-         Element elserveracceptdte =  (Element) nodeserveracceptdte.item(0);  
-         this.serveracceptdte = elserveracceptdte.getFirstChild().getNodeValue();
-         
-
+        
+        Properties prop = new Properties();
+        try (InputStream in = getClass().getResourceAsStream("/appdtews/properties/appdtews.properties")) {
+            prop.load(in);
+       
+          this.pathcert = prop.getProperty("path-certificate");
+          this.pathpdf = prop.getProperty("path-pdf");
+          this.pathcaf = prop.getProperty("path-caf");
+          this.pathdata = prop.getProperty("path-data");
+          this.pathdte = prop.getProperty("path-DTE");
+          this.pathenvironment = prop.getProperty("environment-url");
+          this.pathimg = prop.getProperty("path-img");
+          this.pathtemplate =  prop.getProperty("path-template");
+          this.pathdownload = prop.getProperty("path-download");
+           
+          this.serverauth = prop.getProperty("server-auth");
+          this.serveracceptdte = prop.getProperty("server-acceptdte");
+        }
+       
 }
 
     public String getPathcert() {
--- APPDTEWS/src/java/com/appdte/sii/utilidades/SignDTE.java
+++ APPDTEWS/src/java/com/appdte/sii/utilidades/SignDTE.java
@@ -56,7 +56,7 @@ import org.xml.sax.SAXException;
 
 public class SignDTE {
     
-    public void signDTE(String pathdte,String nombredte,String pathcertificado, String clave) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, KeyStoreException, IOException, CertificateException, UnrecoverableKeyException, UnrecoverableEntryException, KeyException, ParserConfigurationException, SAXException, MarshalException, XMLSignatureException, TransformerConfigurationException, TransformerException{
+    public void signDTE(String pathdte,String nombredte,String certificado, String clave) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, KeyStoreException, IOException, CertificateException, UnrecoverableKeyException, UnrecoverableEntryException, KeyException, ParserConfigurationException, SAXException, MarshalException, XMLSignatureException, TransformerConfigurationException, TransformerException{
                
         
         ConfigAppDTE objConfigAppDTE = new ConfigAppDTE();
@@ -90,7 +90,7 @@ public class SignDTE {
 
         /* instancio el certificado digital */
         KeyStore p12 = KeyStore.getInstance("pkcs12");
-        p12.load(new FileInputStream(objConfigAppDTE.getPathcert()+ pathcertificado), clave.trim().toCharArray());
+        p12.load(new FileInputStream(objConfigAppDTE.getPathcert()+ certificado), clave.trim().toCharArray());
         Enumeration e = p12.aliases();
         String alias = (String) e.nextElement();
         System.out.println("Alias certifikata:" + alias);
